56f23e57a30f6d1624ee47f22358479893e6a0a6
jookly <jookly457@icloud.com>
2021-11-07 16:35:18 +1100

    Search Term Generation Fixed

--- src/Segments/ST_Gen.py
+++ src/Segments/ST_Gen.py
@@ -22,5 +22,33 @@ def search_Term_Generation():
         except RequestException:
             print('Error retrieving google trends json.')
     search_Terms = set(search_Terms)
-    print(search_Terms)
-search_Term_Generation()
+    return search_Terms
+
+temp_terms = search_Term_Generation()
+
+
+# Start Here
+accounts_Using = 5
+set = 3
+instance = 2
+
+# Seperating from pack
+terms = list(temp_terms)
+st_Length = len(terms)
+ct_Allocation = st_Length / accounts_Using
+ct_Allocation = round(ct_Allocation)
+ct_End = ct_Allocation * set
+ct_Start = ct_End - ct_Allocation
+split_Terms = terms[int(ct_Start):int(ct_End)]
+
+# Separating into 3 groups of 5 and discarding the rest
+if instance == 1:
+    print(split_Terms[0:5])
+if instance == 2:
+    print(split_Terms[5:10])
+if instance == 3:
+    print(split_Terms[10:15])
+if instance == 4:
+    print(split_Terms[15:20])
+if instance == 5:
+    print(split_Terms[20:25])
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from os import chmod
 from os.path import isfile
 from sys import exit, platform
 from json import load, loads
@@ -10,13 +9,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from threading import Thread
-<<<<<<< HEAD
-=======
 from random import randint
 import requests
 from requests.exceptions import RequestException
->>>>>>> 75aa6434a4aae769bd36b500c6c0531d5420f2c1
 
 class chrome_Instances():
     def __init__(self):       
@@ -58,14 +53,19 @@ class chrome_Instances():
 
 class Microsoft_Rewards_Automation():
     def __init__(self):
-        self.data_Management()
-        self.platform_Checker()
+        # Variables
         self.ci_1 = []
         self.ci_2 = []
         self.ci_3 = []
         self.ci_4 = []
         self.ci_5 = []
-
+        self.accounts_Using = 5
+        self.search_Terms = []
+        
+        # Functions
+        self.data_Management()
+        self.platform_Checker()
+        self.search_Term_Generation()
 
     def platform_Checker(self):
         self.platform = platform
@@ -113,7 +113,6 @@ class Microsoft_Rewards_Automation():
         for i in range(0, 5):
             date = datetime.now() - timedelta(days=i)
             dates.append(date.strftime('%Y%m%d'))
-        self.search_Terms = []
         for date in dates:
             try:
                 url = f'https://trends.google.com/trends/api/dailytrends?hl=en-US&ed={date}&geo=US&ns=15'
@@ -123,56 +122,29 @@ class Microsoft_Rewards_Automation():
                     self.search_Terms.append(topic['title']['query'].lower())
                     for related_topic in topic['relatedQueries']:
                         self.search_Terms.append(related_topic['query'].lower())
-                sleep(randint(3, 5))
+                sleep(randint(1, 3))
+                print("Starting")
             except RequestException:
                 print('Error retrieving google trends json.')
-
         self.search_Terms = set(self.search_Terms)
-
-    def chrome_Rules(self, email, password, searches, client):
-        #controlls the 1 instance of chrome
-        pass
-
-    def chrome_Thread_Controller(self, set):
-        if set == 1:
-            for i in range(3):
-                self.ci_1.append(chrome_Instances())
-            self.chrome_Rules(self.account_1_email, self.account_1_pass, self.search_Terms, self.ci_1[0])
-            self.chrome_Rules(self.account_1_email, self.account_1_pass, self)
-        if set == 2:
-            for i in range(3):
-                self.ci_2.append(chrome_Instances())
-
-        if set == 3:
-            for i in range(3):
-                self.ci_3.append(chrome_Instances())
-
-        if set == 4:
-            for i in range(3):
-                self.ci_4.append(chrome_Instances())
-
-        if set == 5:
-            for i in range(3):
-                self.ci_5.append(chrome_Instances())
-
-        #controlls the 3 threads inside 
-        #has 3 chrome running under 1 thread
-        pass
-
-    def chrome_Main(self):
-<<<<<<< HEAD
-        Thread(target=self.chrome_Thread_Controller).start()
-        Thread(target=self.chrome_Thread_Controller).start()
-        Thread(target=self.chrome_Thread_Controller).start()
-        Thread(target=self.chrome_Thread_Controller).start()
-        Thread(target=self.chrome_Thread_Controller).start()
-=======
-        Thread(target=self.chrome_Thread_Controller, args=1)
-        Thread(target=self.chrome_Thread_Controller, args=2)
-        Thread(target=self.chrome_Thread_Controller, args=3)
-        Thread(target=self.chrome_Thread_Controller, args=4)
-        Thread(target=self.chrome_Thread_Controller, args=5)
->>>>>>> 75aa6434a4aae769bd36b500c6c0531d5420f2c1
+    
+    def sts(self, set, instance):
+        terms = list(self.search_Terms)
+        st_Length = len(terms)
+        ct_Allocation = st_Length / self.accounts_Using
+        ct_Allocation = round(ct_Allocation)
+        ct_End = ct_Allocation * set
+        ct_Start = ct_End - ct_Allocation
+        split_Terms = terms[int(ct_Start):int(ct_End)]
+        if instance == 1:
+            return split_Terms[0:5]
+        if instance == 2:
+            return split_Terms[5:10]
+        if instance == 3:
+            return split_Terms[10:15]
+        if instance == 4:
+            return split_Terms[15:20]
+        if instance == 5:
+            return split_Terms[20:25]
 
 MSRA = Microsoft_Rewards_Automation()
-MSRA.chrome_Ctrl()
