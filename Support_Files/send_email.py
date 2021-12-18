e98edc906857659216eeb7c7492bef0d4980230d
Jookly <loxdr@protonmail.com>
2021-12-18 21:08:10 +1100

    Email sender Implementation

---
+++ Support_Files/send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+
+def send_email_error(body, subject, email, password):
+    def sende():
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(email, password)
+            mes = f'Subject: {subject}\n\n{body}'
+            smtp.sendmail(email, email, mes)
+    sent = False 
+    while sent == False:
+        try:
+            sende()
+            sent = True
+        except:
+            continue
